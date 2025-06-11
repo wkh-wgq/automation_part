42972d0c103972cb2f8f191ef8d8255a3af3c306
wangkaihua <16621142402@163.com>
2025-06-11 15:36:44 +0800

    添加package.json;修改pokermon注册流程报错

--- app/models/sites/pokermon.rb
+++ app/models/sites/pokermon.rb
@@ -1,4 +1,4 @@
-class Sites::Pokemon < ApplicationRecord
+class Sites::Pokermon < ApplicationRecord
   belongs_to :virtual_user
 
   validates :registry_cellphone, :registry_postcode, :registry_fandi, :reg_password, presence: true
--- app/models/virtual_user.rb
+++ app/models/virtual_user.rb
@@ -2,5 +2,5 @@ class VirtualUser < ApplicationRecord
   validates :email, presence: true
   validates :birthdate, presence: true
 
-  has_one :pokermon, class_name: "Sites::Pokermon", dependent: :destroy
+  has_one :pokermon, class_name: "::Sites::Pokermon", dependent: :destroy
 end
