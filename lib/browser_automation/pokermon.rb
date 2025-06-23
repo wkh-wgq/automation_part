ed666ddeec57322be004a930f268cc77075a177d
wangkaihua <16621142402@163.com>
2025-06-23 11:33:19 +0800

    给方法添加注释

--- lib/browser_automation/pokermon.rb
+++ lib/browser_automation/pokermon.rb
@@ -1,9 +1,11 @@
 module BrowserAutomation
   module Pokermon
+    # 发送注册邮件
     def self.send_register_email(email)
       Pokermon::RegisterRunner.new(email: email).send_email
     end
 
+    # 根据注册链接完成注册
     def self.register(email, register_link, name:, jp_name:, birthday:, gender:, postal_code:, street_number:, password:, mobile:)
       birthday = birthday.to_date if birthday.is_a?(String)
       Pokermon::RegisterRunner.new(email: email).register(
@@ -19,6 +21,7 @@ module BrowserAutomation
       )
     end
 
+    # 抽奖
     def self.draw_lot(email, password:, index:)
       BrowserAutomation::Pokermon::DrawLotRunner.new(
         email: email, password: password, index: index
