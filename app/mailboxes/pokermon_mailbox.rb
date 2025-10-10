24d3115eac6d09bac33e64c4f105e61f111a5dd8
wangkaihua <16621142402@163.com>
2025-10-10 11:31:05 +0800

    修改获取发送时间未转换时区的问题

--- app/mailboxes/pokermon_mailbox.rb
+++ app/mailboxes/pokermon_mailbox.rb
@@ -23,7 +23,7 @@ class PokermonMailbox < ApplicationMailbox
 
   def sent_at
     return @sent_at if @sent_at
-    now = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    now = Time.zone.now.strftime("%Y-%m-%d %H:%M:%S")
     @sent_at = now if has_text_part?
     @sent_at ||= text[/(?:发送时间|Sent)[:：]\s*(\d{4}年\d{1,2}月\d{1,2}日\s*\d{1,2}:\d{2})/, 1]
     @sent_at ||= now
