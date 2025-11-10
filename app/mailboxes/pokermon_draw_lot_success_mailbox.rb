57b5446291ac9fb7196504a12b77de6d48417e4a
wangkaihua <16621142402@163.com>
2025-11-10 18:08:39 +0800

    兼容抽奖成功的邮件内容解析

--- app/mailboxes/pokermon_draw_lot_success_mailbox.rb
+++ app/mailboxes/pokermon_draw_lot_success_mailbox.rb
@@ -10,7 +10,7 @@ class PokermonDrawLotSuccessMailbox < PokermonMailbox
 
   def parse_product_info
     # 允许跨行提取【商品情報】之后的整段文本（最多到下一个空行）
-    product_block = text[/【商品情報】\s*([\s\S]+?)\n\n/, 1]
+    product_block = text[/【商品情報】[\s\u3000]*(.+?)(?:\r?\n){2}/m, 1]
     # 去掉换行符，方便后续正则匹配
     product_line = product_block.gsub(/\s+/, " ")
     if product_line =~ /(\d{13})\u3000(.+?)\u3000([\d,]+)円（\d+個）/
