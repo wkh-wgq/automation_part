25b7b344b67e2201b00eaecc5c1cebbae7727d42
wangkaihua <16621142402@163.com>
2025-11-13 16:45:30 +0800

    调整日志格式；添加日志归档的accessories

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,11 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter   # 使用 application.rb 中定义的 formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
