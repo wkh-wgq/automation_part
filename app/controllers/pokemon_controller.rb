59adca3656aca84b48a916946e8518774f5f2dc7
wangkaihua <16621142402@163.com>
2025-11-17 17:36:33 +0800

    修改存放验证的缓存key

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,13 +1,13 @@
 class PokemonController < ApplicationController
   def captcha
-    captcha = Rails.cache.read("pokermon.#{params[:email]}.login_captcha")
+    captcha = Rails.cache.read("pokemon.#{params[:email]}.login_captcha")
     respond_to do |format|
       format.json { render json: { captcha: captcha } }
     end
   end
 
   def register_link
-    register_link = Rails.cache.read("pokermon.#{params[:email]}.register_link")
+    register_link = Rails.cache.read("pokemon.#{params[:email]}.register_link")
     respond_to do |format|
       format.json { render json: { register_link: register_link } }
     end
